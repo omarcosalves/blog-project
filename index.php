b6b0229b25d2cf1c97d0ff354ab41235752d2b5d
Marcos Alves <102601006+omarcosalves@users.noreply.github.com>
2023-06-10 19:16:20 +0000

    Update body from index.php

--- index.php
+++ index.php
@@ -15,6 +15,59 @@
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+	<!-- aqui inicia o slide -->
+	<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+		<div class="carousel-indicators">
+			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+		</div>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img src="images/img_nature_wide.jpg" class="d-block w-100" alt="...">
+			</div>
+			<div class="carousel-item">
+				<img src="images/img_snow_wide.jpg" class="d-block w-100" alt="...">
+			</div>
+			<div class="carousel-item">
+				<img src="images/img_mountains_wide.jpg" class="d-block w-100" alt="...">
+			</div>
+		</div>
+		<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Previous</span>
+		</button>
+		<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Next</span>
+		</button>
+	</div>
+	<!-- aqui termina o slide -->
+
+	<!-- aqui inicia o grupo de cards -->
+	<div class="container">
+		<div class="row row-cols-1 row-cols-md-3 g-4">
+			<?php foreach($results as $post): ?>
+				<div class="col gy-5">
+					<div class="card">
+						<img src="<?= $post["image"] ?>" class="card-img-top" alt="...">
+						<div class="card-body">
+							<h5 class="card-title">
+								<?= $post["title"] ?>
+							</h5>
+							<p class="card-text">
+								<?= $post["description"] ?>
+							</p>
+							<a href="viewBlog.php?id=<?= $post["id"] ?>">
+								<i class="fas fa-eye check-icon">Ver</i>
+							</a>
+						</div>
+					</div>
+				</div>
+			<?php endforeach; ?>
+		</div>
+	</div>
+<!-- aqui termima o grupo de cards -->
+
 </body>
 </html>
